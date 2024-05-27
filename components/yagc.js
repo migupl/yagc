ac0518dd7c8411bdd56850d34d33698e47dbb041
Miguel Pagola <mipagola@gmail.com>
2024-05-27 11:18:20 +0200

    refactor: duplicate GitHub corner style

--- components/yagc.js
+++ components/yagc.js
@@ -81,6 +81,45 @@ ya-gc {
             }
         }
 
+        #gitHubCornerStyle = () => {
+            const element = document.createElement('style');
+            element.innerHTML = `
+.ya-wc-github-corner:hover .octo-arm {
+    animation: octocat-wave 560ms ease-in-out
+}
+
+@keyframes octocat-wave {
+    0%,
+    100% {
+        transform: rotate(0)
+    }
+
+    20%,
+    60% {
+        transform: rotate(-25deg)
+    }
+
+    40%,
+    80% {
+        transform: rotate(10deg)
+    }
+}
+
+@media (max-width:500px) {
+    .ya-wc-github-corner:hover .octo-arm {
+        animation: none
+    }
+
+    .ya-wc-github-corner .octo-arm {
+        animation: octocat-wave 560ms ease-in-out
+    }
+}
+`
+            return {
+                element
+            }
+        }
+
         #addGithubCornerStyle = () => {
             const styleEl = document.createElement('style');
             styleEl.innerHTML = `
